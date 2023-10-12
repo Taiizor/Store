fff05bc1c72a6ba98d0e80556af88a0daf2476d5
Taiizor <41683699+Taiizor@users.noreply.github.com>
2023-10-12 18:07:15 +0300

    Added "Circular Calendar Display-1" Time

--- src/Time/Glitch Clock-1/js/script.js
+++ src/Time/Glitch Clock-1/js/script.js
@@ -2,24 +2,24 @@ $(document).ready(function() {
 	function second_passed() {
 		$('.clock').removeClass('is-off');
 	}
-
 	setTimeout(second_passed, 2000)
 
 	$('.switcher').on('click', function(e) {
 		e.preventDefault();
 		$('.screen').toggleClass('glitch');
 	});
-});
 
-function SucroseSystemDate(Date) {
-	if (Date.State) {
-		var hours = Date.Hour;
-		var seconds = Date.Second;
-		var minutes = Date.Minute;
+	var newDate = new Date();
+	newDate.setDate(newDate.getDate());
+
+	setInterval(function() {
+		var hours = new Date().getHours();
+		var seconds = new Date().getSeconds();
+		var minutes = new Date().getMinutes();
 
 		var realTime = (hours < 10 ? '0' : '') + hours + ' : ' + (minutes < 10 ? '0' : '') + minutes + ' : ' + (seconds < 10 ? '0' : '') + seconds
 
 		$('.time').html(realTime);
 		$('.time').attr('data-time', realTime);
-	}
-}
+	}, 1000);
+});
