ca6f5a1694b6604d43878b39be6d2a37a38c919b
Taiizor <41683699+Taiizor@users.noreply.github.com>
2024-04-24 07:33:44 +0300

    Updated "Simple Visualizer-1" System

--- src/System/Simple Visualizer-1/JS/viz.js
+++ src/System/Simple Visualizer-1/JS/viz.js
@@ -103,10 +103,10 @@ function renderLine(color) {
 }
 
 function hexToRgb(hex) {
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
 	return result ? {
-		r: parseInt(result[1], 16),
-		g: parseInt(result[2], 16),
-		b: parseInt(result[3], 16)
+		r: parseInt(result[2], 16),
+		g: parseInt(result[3], 16),
+		b: parseInt(result[4], 16)
 	} : null;
 }
